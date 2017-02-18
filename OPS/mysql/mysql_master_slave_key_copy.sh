8bbd1fa06dfcaa7e6ca2e7d64ee7abc5b5969ea1
guolikai <glk73748196@sina.com>
2017-02-18 20:50:42 +0800

    Update mysql_master_slave_key_copy.sh

--- OPS/mysql/mysql_master_slave_key_copy.sh
+++ OPS/mysql/mysql_master_slave_key_copy.sh
@@ -5,6 +5,7 @@
 #Create_Date: 2017-2-18
 #Author:      GuoLikai(glk73748196@sina.com)
 #Description: "Mysql不停主库,批量部署从库脚本"
+#Note:        "需要把主库上的备份文件拷贝过来"
 ##################################################
 MASTER_IP='172.16.1.100'
 MYUSER=root
@@ -30,7 +31,7 @@ $MYSQL_CMD -e "start slave;"
 $MYSQL_CMD -e "show slave status\G"| egrep "IO_Running|SQL_Running" > $LOG_SLAVE
 mail -s "MySQL slave result"  glk73748196@sina.com <$LOG_SLAVE
 #如果执行不成功,可以手动重新做从库
-LOG_FILE=`cat $LOG_FILE | awk '{print $1}'`
-LOG_POS=`cat $LOG_FILE | awk '{print $2}'`
-CHANGE_SQL="CHANGE MASTER TO MASTER_HOST='$MASTER_IP',MASTER_PORT=3306,MASTER_USER='slave',MASTER_PASSWORD='123456',MASTER_LOG_FILE='$LOG_FILE',MASTER_LOG_POS=$LOG_POS;"
-$MYSQL_CMD -e "$CHANGE_SQL"
+#LOG_FILE=`cat $LOG_FILE | awk '{print $1}'`
+#LOG_POS=`cat $LOG_FILE | awk '{print $2}'`
+#CHANGE_SQL="CHANGE MASTER TO MASTER_HOST='$MASTER_IP',MASTER_PORT=3306,MASTER_USER='slave',MASTER_PASSWORD='123456',MASTER_LOG_FILE='$LOG_FILE',MASTER_LOG_POS=$LOG_POS;"
+#$MYSQL_CMD -e "$CHANGE_SQL"
