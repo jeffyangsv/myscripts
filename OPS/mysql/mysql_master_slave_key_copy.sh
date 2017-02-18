abb486edbf5c9d8693b44b18cc93015b9e0e97aa
guolikai <glk73748196@sina.com>
2017-02-18 20:49:12 +0800

    Update mysql_master_slave_key_copy.sh

--- OPS/mysql/mysql_master_slave_key_copy.sh
+++ OPS/mysql/mysql_master_slave_key_copy.sh
@@ -6,6 +6,7 @@
 #Author:      GuoLikai(glk73748196@sina.com)
 #Description: "Mysql不停主库,批量部署从库脚本"
 ##################################################
+MASTER_IP='172.16.1.100'
 MYUSER=root
 MYPASS=123456
 #MYSOCK=/App/data/3306/mysql.sock
@@ -24,12 +25,12 @@ gzip -d mysql_backup_`date +%F`.sql.gz
 $MYSQL_CMD < mysql_backup_`date +%F`.sql
 
 #configure slave
-$MYSQL_CMD -e "CHANGE MASTER TO MASTER_HOST='172.16.1.100',MASTER_PORT=3306,MASTER_USER='slave',MASTER_PASSWORD='123456';"
+$MYSQL_CMD -e "CHANGE MASTER TO MASTER_HOST='$MASTER_IP',MASTER_PORT=3306,MASTER_USER='slave',MASTER_PASSWORD='123456';"
 $MYSQL_CMD -e "start slave;"
 $MYSQL_CMD -e "show slave status\G"| egrep "IO_Running|SQL_Running" > $LOG_SLAVE
 mail -s "MySQL slave result"  glk73748196@sina.com <$LOG_SLAVE
 #如果执行不成功,可以手动重新做从库
 LOG_FILE=`cat $LOG_FILE | awk '{print $1}'`
 LOG_POS=`cat $LOG_FILE | awk '{print $2}'`
-CHANGE_SQL="CHANGE MASTER TO MASTER_HOST='172.16.1.100',MASTER_PORT=3306,MASTER_USER='slave',MASTER_PASSWORD='123456',MASTER_LOG_FILE='$LOG_FILE',MASTER_LOG_POS=$LOG_POS;"
+CHANGE_SQL="CHANGE MASTER TO MASTER_HOST='$MASTER_IP',MASTER_PORT=3306,MASTER_USER='slave',MASTER_PASSWORD='123456',MASTER_LOG_FILE='$LOG_FILE',MASTER_LOG_POS=$LOG_POS;"
 $MYSQL_CMD -e "$CHANGE_SQL"
