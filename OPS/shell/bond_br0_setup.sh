8c6cc8b96564f9c16c992d523927dac94823eb29
guolikai <glk73748196@sina.com>
2017-04-01 20:54:08 +0800

    update 2017-4-1

--- OPS/shell/bond_br0_setup.sh
+++ OPS/shell/bond_br0_setup.sh
@@ -6,18 +6,40 @@
 #Author:      GuoLikai(glk73748196@sina.com)
 #Description: "Centos7上Bond0设置"
 ##################################################
+
+IP=172.16.1.100
+GATEWAY=172.16.1.2
+ETH0=eth0
+ETH1=eth1
+#1.网卡设备名修改
+
+mv /etc/sysconfig/network-scripts/ifcfg-em1{,.bak}
+mv /etc/sysconfig/network-scripts/ifcfg-em2{,.bak}
+
+if [ -f /etc/sysconfig/network-scripts/ifcfg-$ETH0 ];then 
+    cp /etc/sysconfig/network-scripts/ifcfg-$ETH0{,.bak}
+    echo "TYPE=Ethernet
+BOOTPROTO=none
+NAME=$ETH0
+DEVICE=$ETH0
+ONBOOT=yes
+MASTER=bond0" > /etc/sysconfig/network-scripts/ifcfg-$ETH0
+else
+   echo "ifcfg-$ETH0网卡不存在"
+fi
+
+if [ -f /etc/sysconfig/network-scripts/ifcfg-$ETH1 ];then 
+    cp /etc/sysconfig/network-scripts/ifcfg-$ETH1{,.bak}
 echo "TYPE=Ethernet
 BOOTPROTO=none
-NAME=em1
-DEVICE=em1
+NAME=$ETH1
+DEVICE=$ETH1
 ONBOOT=yes
-MASTER=bond0" > /etc/sysconfig/network-scripts/ifcfg-em1
-echo "TYPE=Ethernet
-BOOTPROTO=none
-NAME=em2
-DEVICE=em2
-ONBOOT=yes
-MASTER=bond0" > /etc/sysconfig/network-scripts/ifcfg-em2
+MASTER=bond0" > /etc/sysconfig/network-scripts/ifcfg-$ETH1
+else
+   echo "ifcfg-$ETH1网卡不存在"
+fi
+
 echo "TYPE=Bond
 BOOTPROTO=none
 PEEDNS=yes
@@ -32,9 +54,33 @@ PEEDNS=yes
 NAME=br0
 DEVICE=br0
 ONBOOT=yes
-IPADDR=172.16.1.100
-NETMASK=255.255.255.0
-GATEWAY=172.16.1.2
+IPADDR=$IP
+#NETMASK=255.255.255.0
+PREFIX=24  
+GATEWAY=$GATEWAY
 DNS1=223.5.5.5
 USERCTL=no"  > /etc/sysconfig/network-scripts/ifcfg-br0
 
+#2.编辑内核信息
+if [ -f etc/sysconfig/grub.bak ];then
+    	echo "内核信息已编辑"
+else
+  mv /etc/sysconfig/grub{,.bak}
+  echo 'GRUB_TIMEOUT=5
+  GRUB_DEFAULT=saved
+  GRUB_DISABLE_SUBMENU=true
+  GRUB_TERMINAL_OUTPUT="console"
+  GRUB_CMDLINE_LINUX="crashkernel=auto rhgb net.ifnames=0 biosdevname=0 quiet"
+  GRUB_DISABLE_RECOVERY="true"'   >  /etc/sysconfig/grub
+  grub2-mkconfig -o /boot/grub2/grub.cfg
+fi
+  
+#3.网卡设置成bond1模式
+if [ $( grep "ifenslave bond0 eth0 eth1" /etc/rc.local | wc -l) -eq 0 ];then
+	echo "ifenslave bond0 eth0 eth1" >> /etc/rc.local
+else
+	echo "bond1模式已设置"
+fi
+
+#4.重启network
+systemctl restart network
